1ba01077fc304a7a69fcb9f841fa092308eed681
jeffbhove-at-957077460593 <jeff.bhove@gmail.com>
2019-11-08 07:56:08 -0500

    Added stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,8 +2,6 @@ export default {
   title: 'Demo',
 };
 
-export const heading = () => '<h1>Hello World</h1>';
-
 export const button = () => {
   const btn = document.createElement('button');
   btn.type = 'button';
